e53ddda57d4aa369328c86a2d5223e679de24023
Pauwo <rhernandez35@my.bcit.ca>
2025-03-28 12:04:45 -0700

    testing diff groovy file

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -1,42 +1,97 @@
-def call(dockerRepoName, imageName) {
-  pipeline {
+// def call(dockerRepoName, imageName) {
+//   pipeline {
+//     agent any
+//     stages {
+//       stage('Build') {
+//         steps {
+//             sh 'pip install -r requirements.txt'
+//           }
+//         }
+//       stage('Python Lint') {
+//         steps {
+//           dir(config.servicePath) {
+//             sh 'pylint-fail-under --fail_under 5.0 *.py'
+//           }
+//         }
+//       }
+//       stage('Package') {
+//         when { branch 'main' }
+//         steps {
+//           dir(config.servicePath) {
+//             withCredentials([string(credentialsId: 'pauwo', variable: 'TOKEN')]) {
+//             sh "docker login -u 'pauwo' -p '$TOKEN' docker.io"
+//             sh "docker build -t ${config.dockerRepoName}:latest --tag pauwo/${dockerRepoName}:${imageName} ."
+//             sh "docker push pauwo/${config.dockerRepoName}:${imageName}"
+//             }
+//           } 
+//         }
+//       }
+//       stage('Docker Scan') {
+//         steps {
+//           dir(config.servicePath) {
+//             sh "grype ${config.dockerUsername}/${config.dockerRepoName}:${config.imageTag}"
+//           }
+//         }
+//       }
+//       stage('Deploy') {
+//         steps {
+//           sshagent(credentials: ['SSH_Key_Paulo']) {
+//             sh """
+//               ssh -o StrictHostKeyChecking=no paulo@paulo-acit3855-lab9.eastus.cloudapp.azure.com \
+//                 "docker pull pauwo/${dockerRepoName}:${imageName} && docker compose up -d"
+//             """
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+
+
+def call(dockerRepoName, imageName){
+    pipeline {
     agent any
-    stages {
-      stage('Build') {
-        steps {
-          sh 'pip install -r requirements.txt'
-        }
-      }
-      stage('Python Lint') {
-        steps {
-          sh 'pylint-fail-under --fail_under 5.0 *.py'
-        }
-      }
-      stage('Package') {
-        when { branch 'main' }
-        steps {
-            withCredentials([string(credentialsId: 'pauwo', variable: 'TOKEN')]) {
-            sh "docker login -u 'pauwo' -p '$TOKEN' docker.io"
-            sh "docker build -t ${dockerRepoName}:latest --tag pauwo/${dockerRepoName}:${imageName} ."
-            sh "docker push pauwo/${dockerRepoName}:${imageName}"
+    stages{
+        stage('Build') {
+            steps {
+                sh 'pip install -r requirements.txt'
             }
         }
-      }
-      stage('Docker Scan') {
-        steps {
-          sh "grype pauwo/${dockerRepoName}:${imageName}"
+        stage('Python Lint'){
+            steps{
+                sh 'pylint-fail-under --fail_under 5.0 *.py'
+            }
         }
-      }
-    //   stage('Deploy') {
-    //     steps {
-    //       sshagent(credentials: ['SSH_Key_Paulo']) {
-    //         sh """
-    //           ssh -o StrictHostKeyChecking=no azureuser@acit3855-nginx.eastus2.cloudapp.azure.com \
-    //             "docker pull pauwo/${dockerRepoName}:${imageName} && docker compose up -d"
-    //         """
-    //       }
-    //     }
-    //   }
+
+        stage('Package') {
+            when {
+            expression { env.GIT_BRANCH == 'origin/main' }
+            }
+            steps {
+                withCredentials([string(credentialsId: 'pauwo', variable: 'TOKEN')]) {
+                sh "docker login -u 'pauwo' -p '$TOKEN' docker.io"
+                sh "docker build -t ${dockerRepoName}:latest --tag pauwo/${dockerRepoName}:${imageName} ."
+                sh "docker push pauwo/${dockerRepoName}:${imageName}"
+                }
+            }
+        }
+        stage('Docker Scan') {
+            steps {
+                withCredentials([string(credentialsId: 'pauwo', variable: 'TOKEN')]) {
+                sh "grype pauwo/${dockerRepoName}:${imageName}"
+                }
+                }
+        }
+
+        stage('Deploy') {
+            steps{
+                sshagent(credentials:['SSH_Key_Anson']){
+                    sh "ssh  -o StrictHostKeyChecking=no paulo@paulo-acit3855-lab9.eastus.cloudapp.azure.com docker pull pauwo/${dockerRepoName}:${imageName}"
+                    sh "ssh  -o StrictHostKeyChecking=no paulo@paulo-acit3855-lab9.eastus.cloudapp.azure.com docker compose up -d"
+                }
+            }
+        }
     }
-  }
+
+    }
 }
